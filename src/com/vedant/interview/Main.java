e639de43d0c1e23e7f8e026f2a1342eff956dd1a
Vedant Mathur <vedant@mathur.ca>
2016-12-13 21:19:26 -0800

    Initial Commit

---
+++ src/com/vedant/interview/Main.java
@@ -0,0 +1,109 @@
+package com.vedant.interview;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+    public static Scanner scan = new Scanner (System.in);
+    public static Random rand = new Random();
+    public static void main(String[] args) {
+
+        boolean rangeCorrect = false;
+        boolean correctGuess = false;
+        int minval = 0, maxval = 100;
+        int randomNumberGen = 50, guessCount = 0, guess = 51;
+        int[] UserGuesses = new int[1];
+
+        System.out.println("Username: ");
+        String userName = scan.nextLine();
+
+        System.out.println("Welcome to Guessing Game, " + userName);
+        System.out.println("Please input a range");
+
+        do {
+            //MIMIMUM default is 0
+            System.out.println("Minimum Value: ");
+            String minvalS = scan.nextLine();
+            // Send to check if it's a number
+            if (isANumber(minvalS)){
+                minval = Integer.parseInt(minvalS);
+            }
+
+            //MAXIMUM default is 100
+            System.out.println("Maximum Value: ");
+            String maxvalS = scan.nextLine();
+            if (isANumber(maxvalS)){
+                maxval = Integer.parseInt(maxvalS);
+            }
+
+            //NUMBERCHECK
+            if (minval < maxval && (minval + 10) <= maxval){
+                rangeCorrect = true;
+            } else{
+                    System.out.println("The minimum has to be less then the maximum, and at least 10 numbers apart " + minval + " " + maxval);
+            }
+
+
+        } while(!rangeCorrect);
+        System.out.println("Your minimum value is " + minval + ", and your maximum value is " + maxval);
+
+        randomNumberGen = rand.nextInt(maxval) + minval; //Random number assignment, default is 50
+        System.out.println("I have created a random number between those values. Try to guess!" + randomNumberGen);
+
+        do{
+            System.out.println("Guess: ");
+            String guessS = scan.nextLine();
+            if (isANumber(guessS)) {
+                guess = Integer.parseInt(guessS);
+            }
+
+            guessCount++;
+            addListNum(UserGuesses, guess);
+
+            if (guess == randomNumberGen) correctGuess = true;
+            else if (guess < randomNumberGen) {
+                System.out.println("Too Low! Guess again");
+            }
+            else if (guess > randomNumberGen) {
+                System.out.println("Too High! Guess again");
+            }
+
+            if (guessCount > 10){
+                break;
+            }
+
+        } while (!correctGuess);
+        System.out.println(randomNumberGen + " was correct! It took you " + guessCount + " tries");
+
+
+
+    }
+
+
+
+    public static int[] addListNum (int[] arr, int val) {
+
+        int[] temp = new int [arr.length + 1];
+        for (int i = 0; i <arr.length; i++){
+            temp[i] = arr[i];
+        }
+        temp[arr.length] = val;
+
+        int[] arrdone = temp;
+
+        return arrdone;
+    }
+
+
+
+    public static boolean isANumber (String inRange){
+        try{
+            Integer.parseInt(inRange);
+            return true;
+        }
+        catch(/*GOOGLED*/ NumberFormatException e){
+            return false;
+        }
+    }
+}
